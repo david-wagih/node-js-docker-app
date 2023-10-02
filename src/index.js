a53c17b2a3f8727ad9353d50e32498e24d88b621
David Wagih <78867146+david-wagih@users.noreply.github.com>
2023-10-02 18:39:52 +0300

    add nginx service to redirect requests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const redis = require("redis");
+
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+// Connect to Redis
+const REDIS_PORT = 6379;
+const REDIS_HOST = "redis";
+const redisClient = redis.createClient({
+  url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+});
+redisClient.on("error", (err) => {
+  console.log("Redis error: ", err);
+});
+redisClient.on("connect", () => {
+  console.log("Redis connected");
+});
+
+redisClient.connect();
+ 
 
 // Connect to MongoDB
 const DB_USER = 'root';
@@ -23,9 +41,21 @@ mongoose
     console.log("MongoDB connection error", err);
   });
 
+
+
 app.get("/", (req, res) => {
+  // cache data in redis
+  redisClient.set("products", "products");
   res.send("Hello World from docker compose dev!");
 });
+
+
+app.get("/data", async(req, res) => {
+  const products = await redisClient.get("products");
+  res.send(products);
+}); 
+
+
 
 app.listen(PORT, () => {
   console.log("Server running on port 4000");
