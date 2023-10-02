1857e47c4d6a93a5f7efdb523247b9d007dddb86
David Wagih <78867146+david-wagih@users.noreply.github.com>
2023-10-02 17:38:07 +0300

    connect app container with mongoDB

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,17 @@
-FROM node:18
-
+FROM node:18 as base
 WORKDIR /app
-
 COPY package.json .
 
+
+FROM base as development
 RUN npm install
-
 COPY . .
-
 EXPOSE 4000
-
 CMD ["npm", "run", "start:dev"]
 
+
+FROM base as production
+RUN npm install --only=production
+COPY . .
+EXPOSE 4000
+CMD ["npm", "start"]
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+// Connect to MongoDB
+const DB_USER = 'root';
+const DB_PASSWORD = 'example';
+const DB_PORT = 27017
+const DB_HOST = 'mongo'
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error", err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World from docker compose dev!");
